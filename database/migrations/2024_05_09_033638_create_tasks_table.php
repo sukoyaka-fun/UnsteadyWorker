ababadf4ba10f848cd4eebe02f69e6d964efd443
Misawa <b1018211.fun@gmail.com>
2024-05-13 23:16:37 +0900

    マイグレーションファイルの型指定の誤りを修正

--- database/migrations/2024_05_09_033638_create_tasks_table.php
+++ database/migrations/2024_05_09_033638_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('description');
             $table->BigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->bool('complete_flag');
+            $table->boolean('complete_flag');
             $table->date('due_date');
             $table->timestamps();
         });
